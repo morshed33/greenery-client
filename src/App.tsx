b96d76691e4a71a499bc6cf560a37a8fe8213fe6
morshed33 <ac3m0r@gmail.com>
2024-09-20 13:59:51 +0600

    Initial setup with tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const App: FC = () => {
+  return (
+    <>
+      <div className="text-7xl h-dvh flex items-center justify-center flex-col">
+        <p>Greenery Client</p>
+      </div>
+    </>
+  );
+};
+
+export default App;
